15f8176c4b3bc030c3e3652459748bed3bd6174e
earthhxx <earthhwachirawit@gmail.com>
2025-05-15 14:37:19 +0700

    save

--- src/pages/FilterDepartment.tsx
+++ src/pages/FilterDepartment.tsx
@@ -140,7 +140,7 @@ const App = () => {
           data.some((d) => d.W_NumberID.includes(numid) && d.W_Process === selectedProcess)
         );
         return filteredNumIDs.length > 0 ? (
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+          <div className="flex gap-4">
             {filteredNumIDs.map((numid) => {
               const isSelected = selectedNumberID === numid;
 
@@ -163,6 +163,7 @@ const App = () => {
           </div>
         ) : null;
       })()}
+
       {/* Search Bar */}
       <div className="flex items-center justify-start w-full my-4">
         <div className="w-full lg:w-[40%]">
